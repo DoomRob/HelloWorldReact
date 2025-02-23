c111df5a741aab045e0a771cc8c00cd7d5ef0e66
Robert Gannon <123395116+DoomRob@users.noreply.github.com>
2025-02-23 09:41:33 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
+import ParentComp from './components/ParentComp';
 
-function App() {
-  return (
-    <div className="App">
-      <Table />
-      {/* <FragmentDemo /> */}
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <ParentComp />
+      </div>
+    )
+  }
 }
 
 export default App;
+
