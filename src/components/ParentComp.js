636b22af5ace1f7279993dbb57827200056a8a6f
Robert Gannon <123395116+DoomRob@users.noreply.github.com>
2025-03-02 15:01:31 +0000

    Update ParentComp.js

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,32 +1,35 @@
-import React, { Component, PureComponent } from 'react'
-import RegComp from './RegComp'
-import PureComp from './PureComp'
+import React, { Component } from 'react';
+import RegComp from './RegComp';
+import PureComp from './PureComp';
+import MemoComponent from './MemoComponent';
 
-class ParentComp extends PureComponent {
+class ParentComp extends Component {
 
     constructor(props) {
       super(props)
     
       this.state = {
-         name: 'Mikey'
+         name: 'Robert'
       }
     }
 
-    componentDidMount() {
-        setInterval(() => {
-            this.state({
-                name: 'Mikey'
-            })
-        }, 2000)
+    componentDidMount = () => {
+      setInterval(() => {
+        this.setState({
+          name: 'Robert'
+        })
+      }, 2000)
     }
+    
 
   render() {
     console.log('*************Parent Comp*********************')
     return (
       <div>
         Parent Component
-        <RegComp name={this.state.name}/>
-        <PureComp name={this.state.name}/>
+        <MemoComponent name={this.state.name}/>
+        {/* <RegComp name={this.state.name} /> */}
+        {/* <PureComp name={this.state.name} /> */}
       </div>
     )
   }
