feacaabc6ef6bddaa42c0304f1bb7fbdddc23619
Robert Gannon <123395116+DoomRob@users.noreply.github.com>
2025-03-02 14:58:38 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ParentComp from './components/ParentComp';
+import RefsDemo from "./components/RefsDemo";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <ParentComp />
+        <RefsDemo />
       </div>
     )
   }
