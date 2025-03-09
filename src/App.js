f00c130cb818c7b1858b18ec09b4a70965b6a928
Robert Gannon <123395116+DoomRob@users.noreply.github.com>
2025-03-09 11:06:54 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import RefsDemo from "./components/RefsDemo";
+import InputFocus from "./components/InputFocus";
+import FRParentInput from "./components/FRParentInput";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <RefsDemo />
+       <FRParentInput />
+        {/* <InputFocus /> */}
+        {/* <RefsDemo /> */}
       </div>
     )
   }
