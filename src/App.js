a36c9e0539fd4096a6aa28189dc3d57a2f36fe7d
Robert Gannon <123395116+DoomRob@users.noreply.github.com>
2025-03-16 10:59:29 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import RefsDemo from "./components/RefsDemo";
-import InputFocus from "./components/InputFocus";
-import FRParentInput from "./components/FRParentInput";
+import PortalDemo from "./components/PortalDemo";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-       <FRParentInput />
-        {/* <InputFocus /> */}
-        {/* <RefsDemo /> */}
+       <PortalDemo />
       </div>
     )
   }
